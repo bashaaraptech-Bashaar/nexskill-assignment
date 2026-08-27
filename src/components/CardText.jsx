7e4f54acf90e9525e1bf06039acc219a5bc5d8d4
bashaaraptech-Bashaar <bashaaraptech@gmail.com>
2026-08-28 01:07:46 +0500

    g

--- src/components/CardText.jsx
+++ src/components/CardText.jsx
@@ -17,6 +17,21 @@ function CardText() {
       title: "App Development",
       text: "Create an app from your own business for a more professional business performance.",
     },
+    {
+      icon: <i className="fa-solid fa-pen-ruler"></i>,
+      title: "Graphic Design",
+      text: "Design creative and attractive visuals that give your brand a professional identity.",
+    },
+    {
+      icon: <i className="fa-solid fa-code"></i>,
+      title: "Backend Development",
+      text: "Develop secure and reliable backend systems that keep your applications running smoothly.",
+    },
+    {
+      icon: <i className="fa-solid fa-magnifying-glass-chart"></i>,
+      title: "Digital Marketing",
+      text: "Improve your online presence and reach more customers through effective digital strategies.",
+    },
   ];
 
   return (
